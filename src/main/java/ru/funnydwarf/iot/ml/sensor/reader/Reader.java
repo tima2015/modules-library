8a2c4d8e0697b9f73af389c830c232f1e6cd6706
Timothy Sherstobitov Sergeyevich <woodgoldfilm@gmail.com>
2022-10-09 16:45:40 +0500

    Добавил метод реалиализация которого должна возвращать шаблон массива замеров. Это необходимо для адекватной генерации measurementID

--- src/main/java/ru/funnydwarf/iot/ml/sensor/reader/Reader.java
+++ src/main/java/ru/funnydwarf/iot/ml/sensor/reader/Reader.java
@@ -3,6 +3,16 @@ package ru.funnydwarf.iot.ml.sensor.reader;
 import ru.funnydwarf.iot.ml.sensor.MeasurementData;
 
 public interface Reader {
+    /**
+     * Прочитать показания датчиков устройства
+     * @param address адрес датчика
+     * @return массив замеров для всех датчиков устройства
+     */
     MeasurementData[] read(Object address);
+
+    /**
+     * @return шаблон массива замеров без актуальных данных с датчиков
+     */
+    MeasurementData[] getTemplateRead();
 
 }
