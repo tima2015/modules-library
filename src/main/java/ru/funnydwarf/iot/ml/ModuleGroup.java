9879b5f305886e075ece257170d085ef87e63852
Тимофей Шерстобитов <woodgoldfilm@gmail.com>
2022-10-24 11:07:33 +0300

    Добавил документацию, подредактировал структуру инициализации

--- src/main/java/ru/funnydwarf/iot/ml/ModuleGroup.java
+++ src/main/java/ru/funnydwarf/iot/ml/ModuleGroup.java
@@ -1,24 +1,72 @@
 package ru.funnydwarf.iot.ml;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Группа модулей
+ */
 public abstract class ModuleGroup {
 
+    private final Logger log;
+
+    /**
+     * Состояние инициализации групп модулей
+     */
     enum State {
+        /**
+         * Начальное состояние
+         */
         NOT_INITIALIZED,
+        /**
+         * Ошибка инициализации
+         */
         INITIALIZATION_ERROR,
+        /**
+         * Инициализация успешна
+         */
         OK
     }
-  
+
+    /**
+     * Состояние инициализации группы
+     */
     private State state = State.NOT_INITIALIZED;
 
+    /**
+     * Имя группы
+     */
     private final String name;
+    /**
+     * Описание группы
+     */
     private final String description;
 
     public ModuleGroup(String name, String description) {
+        log = LoggerFactory.getLogger(name);
         this.name = name;
         this.description = description;
     }
 
-    public abstract void initialize() throws Exception;
+    /**
+     * Выполнить инициализацию
+     */
+    public final void initialize() {
+        try {
+            doInit();
+            state = State.OK;
+        } catch (Exception e) {
+            state = State.INITIALIZATION_ERROR;
+            log.error(e.getMessage(), e);
+            throw  new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Выполнение необходимых для инициализации действий
+     * @throws Exception исключение возникшее при инициализации
+     */
+    protected abstract void doInit() throws Exception;
     
     public State getState() {
         return state;
@@ -31,4 +79,6 @@ public abstract class ModuleGroup {
     public String getDescription() {
         return description;
     }
+
+
 }
