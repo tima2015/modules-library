9854ad92aafad3ec3e8a27a7ffa5d14a8ec3959f
Timothy Sherstobitov Sergeyevich <woodgoldfilm@gmail.com>
2022-10-30 15:08:44 +0500

    Я не учёл, что метод afterPropertiesSet выполняется после создания бина Spring-ом, и поэтому полетели все тесты связаные с проверкой состояния инициализации. Я исправил реализовав запуск тестов с участием Spring

--- src/test/java/ru/funnydwarf/iot/ml/ModuleGroupTest.java
+++ src/test/java/ru/funnydwarf/iot/ml/ModuleGroupTest.java
@@ -3,36 +3,63 @@ package ru.funnydwarf.iot.ml;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(classes = ModuleGroupTest.ModuleGroupTestConfiguration.class)
 class ModuleGroupTest {
 
     private static final Logger log = LoggerFactory.getLogger(ModuleGroupTest.class);
 
+    @Configuration
+    static class ModuleGroupTestConfiguration {
+        @Bean("good")
+        ModuleGroup getGoodModuleGroup() {
+            return new ModuleGroup("niceTestGroupName",
+                    "niceTestGroupDescription") {
+                @Override
+                protected InitializationState initialize() throws Exception {
+                    log.debug("initialize() called with: group1 = [{}]", this);
+                    return InitializationState.OK;
+                }
+            };
+        }
+
+        @Bean("bad")
+        ModuleGroup getBadModuleGroup() {
+            return new ModuleGroup("badTestGroupName", "badTestGroupDescription") {
+
+                @Override
+                protected InitializationState initialize() throws Exception {
+                    log.debug("initialize() called with: group1 = [{}]", this);
+                    throw new RuntimeException("this is a bad test");
+                }
+            };
+        }
+    }
+
+    @Autowired
+    @Qualifier("good")
+    private ModuleGroup good;
+
     @Test
     void initialize() {
-        ModuleGroup group = new ModuleGroup("niceTestGroupName",
-                "niceTestGroupDescription") {
-            @Override
-            protected InitializationState initialize() throws Exception {
-                log.debug("initialize() called with: group1 = [{}]", this);
-                return InitializationState.OK;
-            }
-        };
-        assertEquals(group.getInitializationState(), InitializationState.OK);
+        assertEquals(good.getInitializationState(), InitializationState.OK);
     }
+
+    @Autowired
+    @Qualifier("bad")
+    private ModuleGroup bad;
 
     @Test
     void initialize_bad() {
-        ModuleGroup group = new ModuleGroup("badTestGroupName", "badTestGroupDescription") {
-
-            @Override
-            protected InitializationState initialize() throws Exception {
-                log.debug("initialize() called with: group1 = [{}]", this);
-                throw new RuntimeException("this is a bad test");
-            }
-        };
-        assertEquals(group.getInitializationState(), InitializationState.INITIALIZATION_ERROR);
+        assertEquals(bad.getInitializationState(), InitializationState.INITIALIZATION_ERROR);
     }
 }
