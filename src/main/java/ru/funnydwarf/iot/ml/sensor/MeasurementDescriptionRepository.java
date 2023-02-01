abe195b93be00efa4108118a0f436563353fa3c3
Тимофей Шерстобитов <woodgoldfilm@gmail.com>
2023-02-01 08:49:19 +0500

    Добавил метод "найти или создать" в репозиторий описания замеров

--- src/main/java/ru/funnydwarf/iot/ml/sensor/MeasurementDescriptionRepository.java
+++ src/main/java/ru/funnydwarf/iot/ml/sensor/MeasurementDescriptionRepository.java
@@ -7,4 +7,14 @@ public interface MeasurementDescriptionRepository extends CrudRepository<Measure
     @Nullable
     MeasurementDescription findByUnitNameAndNameAndDescription(String unitName, String name, String description);
 
+    static MeasurementDescription findOrCreate(MeasurementDescriptionRepository mdr, String unitName, String name, String description) {
+        MeasurementDescription md = mdr.findByUnitNameAndNameAndDescription(unitName, name, description);
+
+        if (md == null) {
+            md = mdr.save(new MeasurementDescription(unitName, name, description));
+        }
+
+        return md;
+    }
+
 }
