8639027459410039f2882d7b271c62457c0d0f7d
Тимофей Шерстобитов <woodgoldfilm@gmail.com>
2023-03-10 14:24:07 +0500

    Вместо адреса лучше передавать сам модуль

--- src/main/java/ru/funnydwarf/iot/ml/Module.java
+++ src/main/java/ru/funnydwarf/iot/ml/Module.java
@@ -96,11 +96,11 @@ public abstract class Module implements InitializingBean {
      * Инициализация модуля
      */
     protected InitializationState initialize() throws Exception {
-        return initializer != null ? initializer.initialize(address) : InitializationState.OK;
+        return initializer != null ? initializer.initialize(this) : InitializationState.OK;
     }
 
     public static interface Initializer {
-        InitializationState initialize(Object address);
+        InitializationState initialize(Module module);
     }
 
 }
