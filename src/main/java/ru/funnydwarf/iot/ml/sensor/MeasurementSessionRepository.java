a63177d6d8a198affe6ef2a4ff67041f01139ee3
Тимофей Шерстобитов <woodgoldfilm@gmail.com>
2023-03-13 10:38:07 +0500

    Добавил метод получения последней записи в базе данных и статический метод, возвращающий существующую в бд или создающий и помещающий в бд новую сессию.

--- src/main/java/ru/funnydwarf/iot/ml/sensor/MeasurementSessionRepository.java
+++ src/main/java/ru/funnydwarf/iot/ml/sensor/MeasurementSessionRepository.java
@@ -1,8 +1,21 @@
 package ru.funnydwarf.iot.ml.sensor;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MeasurementSessionRepository extends CrudRepository<MeasurementSession, Long> {
+    @Nullable
+    MeasurementSession findFirstByOrderByIdDesc();
+
+    public static MeasurementSession findFirstOrCreate(MeasurementSessionRepository msr) {
+        MeasurementSession session = msr.findFirstByOrderByIdDesc();
+        if (session == null) {
+            session = new MeasurementSession();
+            msr.save(session);
+        }
+        return session;
+    }
 }
