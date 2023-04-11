b36aa691d6f27f0299644b3846203992490bff6c
Тимофей Шерстобитов <woodgoldfilm@gmail.com>
2023-04-11 14:01:33 +0500

    update

--- src/main/java/ru/funnydwarf/iot/ml/sensor/MeasurementRepository.java
+++ src/main/java/ru/funnydwarf/iot/ml/sensor/MeasurementRepository.java
@@ -1,9 +1,14 @@
 package ru.funnydwarf.iot.ml.sensor;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface MeasurementRepository extends CrudRepository<Measurement, Long> {
+    List<Measurement> findByMeasurementDateBetweenOrderByMeasurementDateAsc(Date measurementDateStart, Date measurementDateEnd, Pageable pageable);
 
 }
