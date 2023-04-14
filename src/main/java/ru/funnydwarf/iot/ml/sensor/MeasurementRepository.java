a703a2bc80345e3589207411942cf8ea7e910276
Тимофей Шерстобитов <woodgoldfilm@gmail.com>
2023-04-14 10:42:33 +0500

    update

--- src/main/java/ru/funnydwarf/iot/ml/sensor/MeasurementRepository.java
+++ src/main/java/ru/funnydwarf/iot/ml/sensor/MeasurementRepository.java
@@ -11,4 +11,6 @@ import java.util.List;
 public interface MeasurementRepository extends CrudRepository<Measurement, Long> {
     List<Measurement> findByMeasurementDateBetweenOrderByMeasurementDateAsc(Date measurementDateStart, Date measurementDateEnd, Pageable pageable);
 
+    List<Measurement> findByMeasurementDateBetweenAndMeasurementData_IdOrderByMeasurementDateAsc(Date measurementDateStart, Date measurementDateEnd, long id, Pageable pageable);
+
 }
